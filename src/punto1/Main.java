fc6994bed54df6105d4ce65ac056551917567944
SebastianC <sebas99cano@gmail.com>
2021-05-21 14:38:18 -0500

    implementacion de la funcion variableMayor en la Clase Main del paquete punto1, dando solucion al ejercicio

--- src/punto1/Main.java
+++ src/punto1/Main.java
@@ -2,6 +2,16 @@ package punto1;
 
 public class Main {
     public static void main(String[] args) {
+        variableMayor(67, 90);
+    }
 
+    public static void variableMayor(int variable1, int variable2) {
+        if (variable1 > variable2) {
+            System.out.println("La variable mayor es : " + variable1);
+        } else if (variable2 > variable1) {
+            System.out.println("La variable mayor es : " + variable2);
+        } else {
+            System.out.println("Las variables son iguales");
+        }
     }
 }
---
+++ src/punto1/README.md
@@ -0,0 +1,1 @@
+1. Declara 2 variables numéricas (con el valor que desees), he indica cual es mayor de los dos. Si son iguales indicarlo también. Ve cambiando los valores para comprobar que funciona.
