609f40f390db8235369393b2a9a06a4f0026fe9e
sebastian <sebas99cano@gmail.com>
2021-05-27 14:40:05 -0500

    se creo la clase electrodomestico del paquete punto17.clases con sus respectivos metodos especificados en el README.md

---
+++ src/punto17/clases/Electrodomestico.java
@@ -0,0 +1,116 @@
+package punto17.clases;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Electrodomestico {
+
+    private static final float precioBaseDefecto = 100;
+    private static final String colorDefecto = "blanco";
+    private static final char consumoEnergeticoDefecto = 'F';
+    private static final float pesoDefecto = 5;
+
+    private float precioBase = precioBaseDefecto;
+    private String color = colorDefecto;
+    private char consumoEnergetico = consumoEnergeticoDefecto;
+    private float peso = pesoDefecto;
+
+    public Electrodomestico(float precioBase, String color, char consumoEnergetico, float peso) {
+        this.precioBase = precioBase;
+        this.peso = peso;
+        comprobarConsumoEnergetico(consumoEnergetico);
+        comprobarColor(color);
+    }
+
+    public Electrodomestico(float precioBase, float peso) {
+        this.precioBase = precioBase;
+        this.peso = peso;
+    }
+
+    public Electrodomestico() {
+    }
+
+    private void comprobarConsumoEnergetico(char consumoEnergetico) {
+        List<Character> listaPosible = new ArrayList<>();
+        listaPosible.add('A');
+        listaPosible.add('B');
+        listaPosible.add('C');
+        listaPosible.add('D');
+        listaPosible.add('E');
+
+        this.consumoEnergetico = (listaPosible.stream().anyMatch(s -> s.equals(consumoEnergetico))) ? consumoEnergetico : 'F';
+
+    }
+
+    private void comprobarColor(String color) {
+        List<String> listaPosible = new ArrayList<>();
+        listaPosible.add("negro");
+        listaPosible.add("rojo");
+        listaPosible.add("azul");
+        listaPosible.add("gris");
+
+        this.color = (listaPosible.stream().anyMatch(s -> s.equalsIgnoreCase(color))) ? color : "blanco";
+
+    }
+
+    public float precioFinal() {
+        return precioBase+calcularPrecioConsumoEnergetico()+calcularPrecioPeso();
+    }
+
+    private float calcularPrecioConsumoEnergetico() {
+        float precioAdicional = 0;
+        switch (this.consumoEnergetico) {
+            case 'A':
+                consumoEnergetico = 100;
+                break;
+            case 'B':
+                consumoEnergetico = 80;
+                break;
+            case 'C':
+                consumoEnergetico = 60;
+                break;
+            case 'D':
+                consumoEnergetico = 50;
+                break;
+            case 'E':
+                consumoEnergetico = 30;
+                break;
+            case 'F':
+                consumoEnergetico = 10;
+                break;
+            default:
+                break;
+        }
+        return precioAdicional;
+    }
+
+    private float calcularPrecioPeso() {
+        float precioAdicional = 0;
+        if(this.peso <= 19){
+            precioAdicional = 10;
+        }else if(this.peso <= 49){
+            precioAdicional = 50;
+        }else if(this.peso <= 79){
+            precioAdicional = 80;
+        }else{
+            precioAdicional = 100;
+        }
+        return precioAdicional;
+    }
+
+    public float getPrecioBase() {
+        return precioBase;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public char getConsumoEnergetico() {
+        return consumoEnergetico;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+}
